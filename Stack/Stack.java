423fa85d55d9307ea5b0494f8c1cee1f19cd2ec7
nasciontem <lucas.nascimento@cbcloud.com.br>
2021-01-31 12:08:00 -0300

    Stack algorithm implemented

--- Stack/Stack.java
+++ Stack/Stack.java
@@ -1,5 +1,68 @@
 package Stack;
 
-public class Stack {
+public class Stack<T> {
+    private class Node {
+        private Node node;
+        private T value;
 
+        public Node() {
+            this.node = null;
+            this.value = null;
+        }
+    }
+
+    private Node header;
+
+    public void push(T value) {
+        Node actual = this.header;
+        if (actual == null) {
+            this.header = new Node();
+            this.header.node = new Node();
+            this.header.node.value = value;
+            return;
+        }
+
+        actual = actual.node;
+        while (actual.node != null) {
+            actual = actual.node;
+        }
+
+        actual.node = new Node();
+        actual.node.value = value;
+    }
+
+    public void pop() {
+        Node actual = this.header;
+        if (actual == null) {
+            return;
+        }
+
+        while (actual.node != null && actual.node.node != null) {
+            actual = actual.node;
+        }
+
+        actual.node = null;
+    }
+
+    public T retrieve(int index) {
+        Node actual = this.header;
+        if (actual == null) {
+            return null;
+        }
+
+        actual = actual.node;
+        for (int i = 0; i < index; i++) {
+            if (actual == null) {
+                break;
+            }
+
+            actual = actual.node;
+        }
+
+        if (actual == null) {
+            return null;
+        }
+
+        return actual.value;
+    }
 }
--- Stack/test/StackTest.java
+++ Stack/test/StackTest.java
@@ -1,14 +1,33 @@
 package Stack.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.Test;
+
+import Stack.Stack;
 
 public class StackTest {
     @Test
     public void pushTest() {
+        Stack<String> stack = new Stack<>();
+        stack.push("Primeiro");
+        stack.push("Segundo");
+
+        assertEquals("Primeiro", stack.retrieve(0));
+        assertEquals("Segundo", stack.retrieve(1));
     }
 
     @Test
     public void popTest() {
+        Stack<String> stack = new Stack<>();
+        stack.push("Primeiro");
+        stack.push("Segundo");
 
+        stack.pop();
+        assertEquals("Primeiro", stack.retrieve(0));
+        assertEquals(null, stack.retrieve(1));
+
+        stack.pop();
+        assertEquals(null, stack.retrieve(0));
     }
 }
