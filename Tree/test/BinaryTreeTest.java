980eef8c0f1e8d281eb17d495da3b856aa1cab5f
nasciontem <lucas.nascimento@cbcloud.com.br>
2021-01-31 13:55:17 -0300

    BinaryTree data structure

---
+++ Tree/test/BinaryTreeTest.java
@@ -0,0 +1,23 @@
+package Tree.test;
+
+import org.junit.Test;
+
+import Tree.BinaryTree;
+
+public class BinaryTreeTest {
+    @Test
+    public void insertTest() {
+    }
+
+    @Test
+    public void removeTest() {
+    }
+
+    @Test
+    public void retrieveTest() {
+    }
+
+    @Test
+    public void changeTest() {
+    }
+}
