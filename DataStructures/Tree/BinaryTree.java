ab982e2f09f127ab62cd564e969d467f5e331793
nasciontem <lucas.nascimento@cbcloud.com.br>
2021-02-01 21:12:33 -0300

    BinaryTree structure being made

--- DataStructures/Tree/BinaryTree.java
+++ DataStructures/Tree/BinaryTree.java
@@ -1,5 +1,62 @@
 package DataStructures.Tree;
 
-public class BinaryTree {
+public class BinaryTree<T extends Comparable<T>> {
+    private class Node {
+        private Node leftNode;
+        private Node rightNode;
+        private T value;
 
+        public Node() {
+            this.leftNode = null;
+            this.rightNode = null;
+            this.value = null;
+        }
+    }
+
+    private Node head;
+
+    public void insert(T value) {
+        this.head = this.insert(value, this.head);
+    }
+
+    private Node insert(T value, Node actualNode) {
+        if (actualNode == null) {
+            actualNode = new Node();
+            actualNode.value = value;
+        } else if (value.compareTo(actualNode.value) < 0) {
+            actualNode.leftNode = this.insert(value, actualNode.leftNode);
+        } else if (value.compareTo(actualNode.value) > 0) {
+            actualNode.rightNode = this.insert(value, actualNode.rightNode);
+        }
+
+        return actualNode;
+    }
+
+    public boolean find(T value) {
+        return this.find(value, this.head);
+    }
+
+    private boolean find(T value, Node actualNode) {
+        if (actualNode == null) {
+            return false;
+        } else if (value.compareTo(actualNode.value) < 0) {
+            return this.find(value, actualNode.leftNode);
+        } else if (value.compareTo(actualNode.value) > 0) {
+            return this.find(value, actualNode.rightNode);
+        }
+
+        return true;
+    }
+
+    public void show() {
+        this.show(this.head);
+    }
+
+    private void show(Node actualNode) {
+        if (actualNode != null) {
+            this.show(actualNode.leftNode);
+            System.out.print(actualNode.value + " ");
+            this.show(actualNode.rightNode);
+        }
+    }
 }
--- DataStructures/Tree/test/BinaryTreeTest.java
+++ DataStructures/Tree/test/BinaryTreeTest.java
@@ -1,5 +1,7 @@
 package DataStructures.Tree.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.Test;
 
 import DataStructures.Tree.BinaryTree;
@@ -7,17 +9,28 @@ import DataStructures.Tree.BinaryTree;
 public class BinaryTreeTest {
     @Test
     public void insertTest() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.insert(Integer.valueOf(0));
+        binaryTree.insert(Integer.valueOf(-1));
+        binaryTree.insert(Integer.valueOf(2));
+
+        java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream();
+        System.setOut(new java.io.PrintStream(out));
+
+        binaryTree.show();
+        assertEquals("-1 0 2", out.toString().trim());
     }
 
     @Test
-    public void removeTest() {
-    }
+    public void findTest() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.insert(Integer.valueOf(0));
+        binaryTree.insert(Integer.valueOf(-1));
+        binaryTree.insert(Integer.valueOf(2));
 
-    @Test
-    public void retrieveTest() {
-    }
-
-    @Test
-    public void changeTest() {
+        assert (binaryTree.find(0));
+        assert (binaryTree.find(-1));
+        assert (binaryTree.find(2));
+        assert (!binaryTree.find(4));
     }
 }
